3358af6a4184156118b6bedee213bf907deca32d
suzsy005 <suzsy005@mymail.unisa.edu.au>
2025-08-17 13:52:54 +0930

    I faced some errors and ended up creating a brand new project, so this is my new project.

--- AOWebApp/AOWebApp/Data/AmazonOrders2025Context.cs
+++ AOWebApp/AOWebApp/Data/AmazonOrders2025Context.cs
@@ -32,9 +32,9 @@ public partial class AmazonOrders2025Context : DbContext
 
     public virtual DbSet<Review> Reviews { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=ml-f117-09\\SQLEXPRESS;Initial Catalog=AmazonOrders2025;Integrated Security=True;Encrypt=False;Trust Server Certificate=True");
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Data Source=ml-f117-09\\SQLEXPRESS;Initial Catalog=AmazonOrders2025;Integrated Security=True;Encrypt=False;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- AOWebApp/AOWebApp/Program.cs
+++ AOWebApp/AOWebApp/Program.cs
@@ -1,8 +1,23 @@
+using AOWebApp.Data;
+using Microsoft.Build.Execution;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AmazonOrders2025Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AmazonOrders2025Context") ??
+    throw new InvalidOperationException("Connection string 'AOWebAppContext' not found.")));
+
+//// AmazonOrdersContext
+//builder.Services.AddDbContext<AmazonOrdersContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("AmazonOrdersContext") ??
+//    throw new InvalidOperationException("Connection string 'AmazonOrdersContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+
 
 var app = builder.Build();
 
