435eb2fdb3eb94eb6c94e464a697a482557a0607
suzsy005 <suzsy005@ML-F117-09>
2025-08-17 13:06:01 +0930

    Add project files.

---
+++ AOWebApp/AOWebApp/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AOWebApp.Models;
+
+public partial class Customer
+{
+    public int CustomerId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string MainPhoneNumber { get; set; } = null!;
+
+    public string? SecondaryPhoneNumber { get; set; }
+
+    public int AddressId { get; set; }
+
+    public virtual Address Address { get; set; } = null!;
+
+    public virtual ICollection<CustomerOrder> CustomerOrders { get; set; } = new List<CustomerOrder>();
+
+    public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
+}
---
+++ AOWebApp/AOWebApp/Models/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AOWebApp.Models;
+
+public partial class Item
+{
+    public int ItemId { get; set; }
+
+    public string ItemName { get; set; } = null!;
+
+    public string ItemDescription { get; set; } = null!;
+
+    public decimal ItemCost { get; set; }
+
+    public string ItemImage { get; set; } = null!;
+
+    public int CategoryId { get; set; }
+
+    public virtual ItemCategory Category { get; set; } = null!;
+
+    public virtual ICollection<ItemMarkupHistory> ItemMarkupHistories { get; set; } = new List<ItemMarkupHistory>();
+
+    public virtual ICollection<ItemsInOrder> ItemsInOrders { get; set; } = new List<ItemsInOrder>();
+
+    public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
+}
