01ceb97eb972cb0fd09ffd88ccba720d5a2ffa71
suzsy005 <suzsy005@mymail.unisa.edu.au>
2025-08-22 16:00:43 +0930

    Create View Model(ItemSearchViewModel.cs)

---
+++ AOWebApp/AOWebApp/ViewModels/ItemSearchViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using AOWebApp.Models;
+using System.Collections.Generic;
+
+namespace AOWebApp.ViewModels
+{
+    public class ItemSearchViewModel
+    {
+        public string SearchText { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public SelectList CategoryList { get; set; }
+
+        public List<Item> ItemList { get; set; }
+    }
+}
