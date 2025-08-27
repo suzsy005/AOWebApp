48261ba53f6ca68ce933ecc1a83491ca1b929855
suzsy005 <suzsy005@mymail.unisa.edu.au>
2025-08-27 15:45:25 +0930

    Add CustomerSearchViewModel

---
+++ AOWebApp/AOWebApp/ViewModels/CustomerSearchViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using AOWebApp.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace AOWebApp.ViewModels
+{
+    public class CustomerSearchViewModel
+    {
+        public string SearchText { get; set; }
+
+        public string Suburb { get; set; }
+
+        public SelectList SuburbList { get; set; }
+
+        public List<Customer> CustomerList { get; set; }
+
+        public List<string> NameList { get; set; }
+    }
+}
