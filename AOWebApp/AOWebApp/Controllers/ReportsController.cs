0a59c8d792cb882ff338bcd5b433a2b37c86484f
suzsy005 <suzsy005@mymail.unisa.edu.au>
2025-10-23 15:18:52 +1030

    Added report json

--- AOWebApp/AOWebApp/Controllers/ReportsController.cs
+++ AOWebApp/AOWebApp/Controllers/ReportsController.cs
@@ -23,5 +23,32 @@ namespace AOWebApp.Controllers
 
 			return View("AnnualSalesReport", new SelectList(yearList));
 		}
+
+		[Produces("application/json")]
+		public IActionResult AnnualSalesReportData(int Year)
+		{
+			if (Year == 0)
+			{
+				var yearMatch = _context.ItemsInOrders
+					.Where(iio => iio.OrderNumberNavigation.OrderDate.Year == Year)
+					.GroupBy(iio => new { iio.OrderNumberNavigation.OrderDate.Year, iio.OrderNumberNavigation.OrderDate.Month })
+					.Select(group => new
+					{
+						year = group.Key.Year,
+						monthNo = group.Key.Month,
+						monthName = "",
+						totalItems = group.Sum(iio => iio.NumberOf),
+						totalSales = group.Sum(iio => iio.TotalItemCost),
+					})
+					.OrderBy(data => data.monthNo);
+
+				return Json(null);
+			}
+			else
+			{
+				return BadRequest();
+			}
+		}
+
 	}
 }
