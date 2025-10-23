0817886b6ce7192e4a6a6f43ba76674ef0ee6e54
suzsy005 <suzsy005@mymail.unisa.edu.au>
2025-10-23 13:47:53 +1030

    Re-work on Week 10 Actitity
    I'm doing Week 10 activity again because I've using the file provided by Mark from this Week 10 but suddenly I couldn't connect the data to project on Package Manager Console, keep receiving error and I gave up on using that file so I started using my file from week 4 and doing from week 10 activity now

---
+++ AOWebApp/AOWebApp/Controllers/ReportsController.cs
@@ -0,0 +1,20 @@
+﻿using AOWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AOWebApp.Controllers
+{
+	public class ReportsController : Controller
+	{
+		private readonly AmazonOrders2025Context _context;
+
+		public ReportsController(AmazonOrders2025Context context) 
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			return View("AnnualSalesReport");
+		}
+	}
+}
