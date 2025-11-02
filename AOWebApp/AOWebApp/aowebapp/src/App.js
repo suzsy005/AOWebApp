84fea035fadfd08eb909151ed44dd78be2fdc8d0
suzsy005 <suzsy005@mymail.unisa.edu.au>
2025-11-02 17:46:48 +1030

    Done setting navbar

--- AOWebApp/AOWebApp/aowebapp/src/App.js
+++ AOWebApp/AOWebApp/aowebapp/src/App.js
@@ -1,18 +1,31 @@
-//import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card'
 import CardV2 from './components/CardV2'
 import CardV3 from './components/CardV3'
 import CardList from './components/CardListSearch'
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App container">
-        <div className="bg-light py-1 mb-2">
-            <h2 className="text-center">
-                Example Application
-            </h2>
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-ligth">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">AOWebApp</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNalAltMarkup">
+                    <div className="navbar-nav">
+                        <Link className="nav-link active" to="/Home">Home</Link>
+                        <Link className="nav-link" to="/Contact">Contact</Link>
+                        <Link className="nav-link" to="/Products">Products</Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+
+
         <div className="row justify-content-center">
               <Card
                   itemId="1"
